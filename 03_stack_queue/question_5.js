3dd6c4cce0f27cc5dbfcd55dc92e025e8dcfaba4
MoKyungKim <monee.kim@legendaries.team>
2024-02-05 21:30:48 +0900

    주석 제거

--- 03_stack_queue/question_5.js
+++ 03_stack_queue/question_5.js
@@ -1,14 +1,14 @@
 // Q. 가장 작은 값이 위로 오도록 스택을 정렬하는 프로그램
 
 function sortStack(stack) {
-    let helperStack = []; // 두 번째 스택을 도우미 스택으로 사용
+    let helperStack = []; // 도우미 스택
 
     while(stack.length > 0) {
-        // 현재 스택에서 하나의 원소를 꺼냄
         let temp = stack.pop();
         
         // 도우미 스택에서 현재 원소보다 큰 원소들을 모두 원래 스택으로 옮김
-        while(helperStack.length > 0 && helperStack[helperStack.length - 1] < temp) {
+        while(helperStack.length > 0 
+            && helperStack[helperStack.length - 1] < temp) {
             stack.push(helperStack.pop());
         }
         
@@ -22,7 +22,6 @@ function sortStack(stack) {
     }
 }
 
-// 사용 예
 let stack = [34, 3, 31, 98, 92, 23];
 sortStack(stack);
 console.log("Sorted Stack:", stack);
