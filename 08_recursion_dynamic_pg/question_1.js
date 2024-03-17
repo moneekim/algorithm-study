fb9d4c01e6e409bc1b0a3b344a36961807a082b5
MoKyungKim <monee.kim@legendaries.team>
2024-03-17 19:07:17 +0900

    재귀 문제 8.1, 8.8 풀이

---
+++ 08_recursion_dynamic_pg/question_1.js
@@ -0,0 +1,44 @@
+/**
+ * Q. 어떤 아이가 n개의 계단을 오른다.
+ * 한 번에 1계단 오르기도 하고， 2계단이나 3계단을 오르기도 한다.
+ * 계단을 오르는 방법이 몇 가지가 있는지 계산하는 메서드를 구현하라.
+ */
+
+// i. brute-force 방법
+/**
+n번째 계단을 오르는 경우는 다음 중 하나가 된다.
+- (n-l)번째 계단에서 한 계단 올라가기
+- (n-2)번째 계단에서 두 계단 올라가기
+- (n-3)번째 계단에서 세 계단 올라가기
+ */
+function countWays(n) {
+  if (n < 0) {
+    return 0;
+  } else if (n === 0) {
+    return 1;
+  } else {
+    return countWays(n - 1) + countWays(n - 2) + countWays(n - 3);
+  }
+}
+
+// ii. 메모이제이션 방법
+function countWays_memoization(n) {
+  const memo = new Array(n + 1).fill(-1);
+  return countWaysHelper(n, memo);
+}
+
+function countWaysHelper(n, memo) {
+  if (n < 0) {
+    return 0;
+  } else if (n === 0) {
+    return 1;
+  } else if (memo[n] > -1) {
+    return memo[n];
+  } else {
+    memo[n] =
+      countWaysHelper(n - 1, memo) +
+      countWaysHelper(n - 2, memo) +
+      countWaysHelper(n - 3, memo);
+    return memo[n];
+  }
+}
---
+++ 08_recursion_dynamic_pg/question_8.js
@@ -0,0 +1,44 @@
+/**
+ * Q. 문자열이 주어졌을 때 모든 경우의 순열을 계산하는 메서드를 작성하라.
+ * 문자는 중복되어 나타날 수 있지만， 나열된 순열은 서로 중복되면 안된다.
+ */
+
+function printPerms(s) {
+  let result = [];
+  let map = buildFreqTable(s);
+  generatePerms(map, "", s.length, result);
+  return result;
+}
+
+// 문자열 내 각 문자의 빈도수를 계산하여 Map 객체로 반환
+function buildFreqTable(s) {
+  let map = new Map();
+  for (let c of s) {
+    if (!map.has(c)) {
+      map.set(c, 0);
+    }
+    map.set(c, map.get(c) + 1);
+  }
+  return map; // s가 aab map = Map(2) { 'a' => 2, 'b' => 1 }
+}
+
+// 재귀적으로 순열을 생성하며, 각 문자의 사용 횟수를 추적하여 중복 생성을 방지
+function generatePerms(map, prefix, remaining, result) {
+  // 순열이 완성되었을 경우
+  if (remaining === 0) {
+    result.push(prefix);
+    return;
+  }
+  // 남은 문자들을 이용해서 나머지 순열을 생성
+  for (let [c, count] of map.entries()) {
+    if (count > 0) {
+      map.set(c, count - 1); // 빈도 업데이트
+      generatePerms(map, prefix + c, remaining - 1, result);
+      // 재귀 호출 후에는 원래 문자의 카운트를 복원
+      map.set(c, count);
+    }
+  }
+}
+
+console.log(printPerms("abc"));
+console.log(printPerms("aab"));
